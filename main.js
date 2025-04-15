500958c50257c12a84fe8456ce11062051208c8b
MrAdeel6t6 <mrtecnical13@gmail.com>
2025-04-15 09:19:47 +0500

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 var PasswordPannel = document.querySelector("#password-panel");
+
+document.querySelector(".visible-icon").addEventListener("click", (e)=>{
+  var check = document.querySelector(`.visible-icon img`);
+  if (check.src.includes("eye-line.svg")) {
+    check.src = "eye-off-line.svg";
+    PasswordPannel.type = "text"
+  } else {
+    check.src = "eye-line.svg";
+    PasswordPannel.type = "password"
+  }
+});
